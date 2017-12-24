85bf2aaecdbc18e6900fdc75326394030f95ab9c
drdlee <danielrudi.lee@gmail.com>
2017-12-25 06:23:11 +0800

    Reducer: its like a state in react

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+  return [
+    {title: 'JavaScript: the weird parts'},
+    {title: 'Harry Potter'},
+    {title: 'Narnia'},
+    {title: 'Doaraemon: dunia alam nyata'},
+    {title: 'Jurrasic World: part III'},
+  ]
+}
