e1a5dff97ec1a0e84148a5a192692810a50f44be
drdlee <danielrudi.lee@gmail.com>
2017-12-25 20:33:43 +0800

    adding the first test action creator

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {selectBook} from '../actions/index'; //import the action function we make
+import {bindActionCreators} from 'redux'; // for binding the function to the state
 
 class BookList extends Component {
   renderList(){
     return this.props.books.map((book)=>{
-      return <li key={book.title} className="list-group-item">{book.title}</li>
+      return (
+        <li
+          key={book.title}
+          onClick={()=> this.props.selectBook(book)} // this will run the selectBook function
+          className="list-group-item">
+            {book.title}
+        </li>)
     })
   }
 
@@ -24,4 +32,8 @@ function mapStateToProps(state){
 } // this function is for making a state from redux reducer,
   // books: is the state that will be used in react, state.books is the value from redux reducers
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({selectBook: selectBook}, dispatch)
+} //this function will bind the action imported above(selectBook), to a new property of selectBook
+  // and pass it to every object in the state also this will be available in the state same as books
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
