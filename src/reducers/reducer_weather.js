8af7a5e5b5107249075fee8a6e88b9fe6e24fd64
drdlee <danielrudi.lee@gmail.com>
2017-12-28 00:19:42 +0800

    fixing some reducer returned data and clean up google_map

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -2,9 +2,16 @@ import {FETCH_WEATHER} from '../actions/index';
 
 export default function(currentState = [], action){
   console.log('Action received in Reducers:', action); // here the promise from the Action already converted to data by the middleware redux-promise
-  switch (action.type){
-  case FETCH_WEATHER:
-    return [action.payload.data, ...currentState]; // cara ES6, ini masukin data baru kedalam isi data lama
+  // switch (action.type){
+  // case FETCH_WEATHER:
+  //   return [action.payload.data, ...currentState]; // cara ES6, ini masukin data baru kedalam isi data lama
+  // }
+  if (action.type === FETCH_WEATHER){
+    if(action.error){
+      return currentState;
+    }else{
+      return [action.payload.data, ...currentState];
+    }
   }
-  return currentState
+  return currentState;
 }
