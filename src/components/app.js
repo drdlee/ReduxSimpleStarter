0aefe6d5e24bca83acc1012831b6313c895ea8ff
drdlee <danielrudi.lee@gmail.com>
2017-12-28 06:45:39 +0800

    adding Routes

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>Initial Commit</div>
+      <div>
+        APPJS
+        {this.props.children}
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory} />
+    <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));
