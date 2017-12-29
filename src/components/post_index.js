fec0a0d10bdecba18ed86326ff1988197d8edf06
drdlee <danielrudi.lee@gmail.com>
2017-12-30 07:51:09 +0800

    adding lifeCycle for the Action will be called, and some shortcuts for calling the Action

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
-export default () => {
-  return <div>List of Index</div>;
+class PostIndex extends Component {
+  componentWillMount(){
+    this.props.fetchPost();  // remember to use () because function will run, if not use () function will not run
+  }
+
+  render(){
+    return (
+      <div>List of blog posts</div>
+    );
+  }
 }
+
+// function mapDispatchtoProps(dispatch){
+//   return bindActionCreators({ fetchPost }, dispatch);
+// }  //  no need to use this function, we can just directly call it from export default connect below, so we dont need to import bindActionCreators anymore.
+
+export default connect (null, { fetchPost })(PostIndex);  //  null, is the function for mapStatetoProps, because we dont have it now, so.. null.
+//  { fetchPost } is short hand for { fetchPost: fetchPost } the first fetchPost is the object we want to use here (this.props.fetchPost)
+//  the second fetchPost is the function we export from Action and we import here with the var fetchPost above
