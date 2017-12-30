6058a886a5f7a08681232cf2f9c1b6f8135b5422
drdlee <danielrudi.lee@gmail.com>
2017-12-30 08:02:06 +0800

    Adding Route and Link to Create a new post

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostIndex extends Component {
   componentWillMount(){
@@ -9,7 +10,14 @@ class PostIndex extends Component {
 
   render(){
     return (
-      <div>List of blog posts</div>
+      <div>
+        <div className="text-xs-right">
+          <Link to="/post/new" className="btn btn-primary">
+            Add New Post
+          </Link>
+        </div>
+        List of blog posts
+      </div>
     );
   }
 }
